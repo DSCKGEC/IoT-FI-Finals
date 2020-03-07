3136692daa025b50d7460d1d0ab8d7d59c840859
Ayushman Bilas Thakur <ayushmanbilasthakur@gmail.com>
2020-03-07 11:30:34 +0530

    Initial changes

--- index.js
+++ index.js
@@ -12,20 +12,52 @@ const devices = [];
 // this is how to create a new on off based device
 const newDevice = new OnOffDevice(devices.length, "Device 001");
 
+//this is how to add your new device
+devices.push(newDevice);
 
+console.log(devices);
 
 // this is a post request to the route "/:id"
 // this route will be to get the object with the given id
 app.get("/:id", (req, res) => {
+    //extracting the id form request parameters
+    const id = req.params.id;
+
     //the first device is selected which has the mentioned id and is in the devices list
-    const device = devices.filter(item => item.id === id);
+    const device = devices.filter(item => item.id !== id);
 
     if (device.length < 1)
         res.status(404).json({
-            message: "Could not find the data"
+            message: `Could not find the device with ID ${id}`
         });
 
-    res.status(200).json(device[0]);
+    else {
+        res.status(200).json({
+            device: device[0]
+        });
+    }
+});
+
+//this is for turning a device on and off
+app.get("/toggle_status/:id", (req, res) => {
+    //extracting the id form request parameters
+    const id = req.params.id;
+
+    //the first device is selected which has the mentioned id and is in the devices list
+    const device = devices.filter(item => item.id !== id);
+
+    if (device.length < 1)
+        res.status(404).json({
+            message: `Could not find the data of device with id ${id}`
+        });
+
+    else {
+        device[0].toggleStatus();
+        res.status(200).json({
+            id,
+            status: device[0].status
+        });
+    }
 });
 
 app.listen(PORT, () => {
--- models/on_off_device.js
+++ models/on_off_device.js
@@ -3,9 +3,14 @@ class OnOffdDevice {
     //here is a list of all the possible fields for an IoT device
     device_id = 0;
     device_name = "default";
+    //false means off true means on
+    device_status = false;
     constructor(id, device_name) {
         this.device_id = id;
         this.device_name = device_name;
+    }
+    toggleStatus = () => {
+        this.device_status = !this.device_status;
     }
 }
 
